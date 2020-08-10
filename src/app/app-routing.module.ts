6c84079616431094353c53a7d22e35de4bb5c08b
K.Bonev-Wagner <kerstin-wagner@gmx.at>
2020-08-10 20:51:04 +0200

    [feat] simplified routing and elegant scrolling to menu item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'Home', component: HomeComponent },
-  { path: 'Login', component: LoginComponent },
-  { path: 'About', component: AboutComponent },
-  { path: 'Register', component: RegisterComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
