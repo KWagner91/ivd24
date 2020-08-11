9a99fecc2359c191047dbb148e8d7083293d2e3d
K.Bonev-Wagner <kerstin-wagner@gmx.at>
2020-08-11 17:59:03 +0200

    [feat] example content with bullet points and different style examples

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,3 @@
-import { RegisterComponent } from './register/register.component';
-import { AboutComponent } from './about/about.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
