f0037759e9451dac33322c0da08c770ed66a79a7
K.Bonev-Wagner <kerstin-wagner@gmx.at>
2020-08-13 19:31:13 +0200

    [feat] register form backbone created

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  registerForm = new FormGroup({
+    gender: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormControl(''),
+    postCode: new FormControl(''),
+    city: new FormControl(''),
+    country: new FormControl(''),
+    company: new FormControl(''),
+    phone: new FormControl(''),
+    mobilePhone: new FormControl(''),
+    fax: new FormControl(''),
+    email: new FormControl(''),
+    userName: new FormControl(''),
+    password: new FormControl(''),
+    passwordRepeat: new FormControl(''),
+    agb: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(JSON.stringify(this.registerForm.value));
   }
-
 }
