6d4963a27b2f49871012665d9019a1ea3af1fdc8
K.Bonev-Wagner <kerstin-wagner@gmx.at>
2020-08-15 09:59:41 +0200

    [feat] Tested Angular HTTP Client with PHP backend sending data to MySQL database

---
+++ backend/api/database.php
@@ -0,0 +1,22 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'ivd24');
+define('DB_NAME', 'userdata');
+
+function connect()
+{
+  $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
+
+  if (mysqli_connect_errno($connect)) {
+    die("Failed to connect:" . mysqli_connect_error());
+  }
+  mysqli_set_charset($connect, "utf8");
+  return $connect;
+}
+
+$con = connect();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +19,14 @@ import { RegisterComponent } from './register/register.component';
     LoginComponent,
     AboutComponent,
     RegisterComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
